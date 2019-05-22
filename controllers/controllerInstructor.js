e22590fcaa601083b65370fd5ce49d56cc2c6edb
robbycp <robbycaesar@gmail.com>
2019-05-22 14:24:36 +0700

    index and edit view page

--- controllers/controllerInstructor.js
+++ controllers/controllerInstructor.js
@@ -2,15 +2,57 @@ const Models = require('../models')
 
 class ControllerInstructor {
   static indexGet(req,res) {
-    
+    let instructorId = req.params.id
+    let input = {
+      title: 'Profile',
+      action: 'showData',
+      id: instructorId,
+      dataInstructor: {
+        name: 'Budi',
+        username: 'budi123'
+      },
+      dataStudentPending: [{
+        username: 'anto',
+        motivasi: 'loremipsum',
+        status: 'pending'
+      }],
+
+      dataStudentAccept: [{
+        username: 'siapa kamu',
+        motivasi: 'loremipsum lerem ipsum dolor et labora',
+        status: 'accept'
+      }]
+    }
+    res.render('profile', input)
   }
 
   static indexPost(req,res) {
-    
+
   }
 
   static editGet(req,res) {
-    
+    let instructorId = req.params.id
+    let input = {
+      title: 'Profile',
+      action: 'edit',
+      id: instructorId,
+      dataInstructor: {
+        nama: 'Budi',
+        username: 'budi123'
+      },
+      dataStudentPending: [{
+        username: 'anto',
+        motivasi: 'loremipsum',
+        status: 'pending'
+      }],
+
+      dataStudentAccept: [{
+        username: 'siapa kamu',
+        motivasi: 'loremipsum lerem ipsum dolor et labora',
+        status: 'accept'
+      }]
+    }
+    res.render('profile', input)
   }
 
   static editPost(req,res) {
--- views/gutenwp-free-gutenberg-compatible-wordpress-blog-theme/unzip_first_gutenwp_v_1_0_1/theme-pack/gutenwp/js/admin-colorpicker.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function($){
-    $('.gutenwp-color-picker').wpColorPicker();
-});
